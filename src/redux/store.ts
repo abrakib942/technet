b034b2896976c47549e54794200008581e31f6f6
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-07-10 23:06:07 +0600

    product query and mutation/ adding rtk query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cart from './features/cart/cartSlice';
 import product from './features/product/productSlice';
+import { api } from './api/apiSlice';
 
 const store = configureStore({
   reducer: {
     cart,
     product,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
